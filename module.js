9a6f8c4fd2d42ab30f11c33713cc7e1aa6d1a42d
Obele Austin <obeleoyeinfie@gmail.com>
2023-09-28 12:21:32 +0100

    alls work

--- module.js
+++ module.js
@@ -6,17 +6,11 @@ let Population = [] ;
 let slugNation = [];
 let Year = []
 let th1 = document.createElement('th')
-th1.innerHTML = 'ID Nation'
 let th2 = document.createElement('th')
-th2.innerHTML = 'ID Year'
 let th3 = document.createElement('th')
-th3.innerHTML = 'Nation'
 let th4 = document.createElement('th')
-th4.innerHTML = 'Population'
-let th5 = document.createElement('th')
-th5.innerHTML = 'Slug Nation'
 let th6 = document.createElement('th')
-th6.innerHTML = 'Year'
+let th5 = document.createElement('th')
 
 export{table, idNation,idYear,Nation,
     Population,slugNation,Year,th1,th2,th3,th4,th5,th6}
